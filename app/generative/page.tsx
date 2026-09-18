3843c3c201024b577c2f7c3bffbda8dddccfe11a
AliLin200446 <cl7578@nyu.edu>
2026-09-18 01:10:51 -0400

    Show Diesel Fashion Film first on Generative

--- app/generative/page.tsx
+++ app/generative/page.tsx
@@ -20,8 +20,8 @@ export default function GenerativePage() {
           GENERATIVE
         </h1>
       </header>
-      <LelaboStudies />
       <FashionFilm assets={{ video: available("film.mp4"), poster: available("poster.jpg"), stills: [1, 2, 3, 4].map((id) => available(`gs${id}.jpg`)) }} />
+      <LelaboStudies />
     </main>
   );
 }
